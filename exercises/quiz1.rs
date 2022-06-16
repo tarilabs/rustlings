40d6caf7949ff0da9914c1fa0fc8507b49d3d26e
tarilabs <matteo.mortari@gmail.com>
2022-06-16 21:03:54 +0200

    backup

--- exercises/quiz1.rs
+++ exercises/quiz1.rs
@@ -8,10 +8,11 @@
 // more than 40 at once, each apple only costs 1! Write a function that calculates
 // the price of an order of apples given the quantity bought. No hints this time!
 
-// I AM NOT DONE
-
 // Put your function here!
-// fn calculate_apple_price {
+fn calculate_apple_price(a : u32) -> u32 {
+    let unit_price = if a > 40 { 1 } else { 2 };
+    a * unit_price
+}
 
 // Don't modify this function!
 #[test]
--- exercises/structs/structs2.rs
+++ exercises/structs/structs2.rs
@@ -1,8 +1,6 @@
 // structs2.rs
 // Address all the TODOs to make the tests pass!
 
-// I AM NOT DONE
-
 #[derive(Debug)]
 struct Order {
     name: String,
@@ -26,6 +24,15 @@ fn create_order_template() -> Order {
     }
 }
 
+fn my_create_order() -> Order {
+    let ord = create_order_template();
+    Order {
+        name: String::from("Hacker in Rust"),
+        count: 1,
+        ..ord
+    }
+}
+
 #[cfg(test)]
 mod tests {
     use super::*;
@@ -34,7 +41,7 @@ mod tests {
     fn your_order() {
         let order_template = create_order_template();
         // TODO: Create your own order using the update syntax and template above!
-        // let your_order =
+        let your_order = my_create_order();
         assert_eq!(your_order.name, "Hacker in Rust");
         assert_eq!(your_order.year, order_template.year);
         assert_eq!(your_order.made_by_phone, order_template.made_by_phone);
